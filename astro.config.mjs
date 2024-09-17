f68ab65f05b467fe97eb11d741dd3ec84c9efffd
LittleHaku <ivandelhorno@gmail.com>
2024-09-17 14:32:12 +0200

    image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
+});
+
+adapter: vercel({
+  imageService: true,
 });
